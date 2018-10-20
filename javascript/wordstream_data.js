8bde062a8296118f1d5a04f942e2b0e50b367fd6
phamvanvung <phamvanvung@gmail.com>
2018-10-20 06:04:52 +0200

    Reserve phrases

--- javascript/wordstream_data.js
+++ javascript/wordstream_data.js
@@ -7,7 +7,8 @@ function loadNewsData(rawData, draw) {
     wordAuthors = {};
     //<editor-fold desc="process stopwords">
     let stopWords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"];
-    let removeWords = ["iot", "internet", "things", "--","can", "will", "new", "hn", "data", "ask","pdf", "10", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"];
+    let removeWords = ["iot", "internet", "things", "--","can", "will", "new", "hn", "ask","pdf", "10", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"];
+    let phrases = ["Big Data", "Open Source"];
     stopWords = stopWords.concat(removeWords);
 
     function sanitizeWord(word) {
@@ -68,8 +69,27 @@ function loadNewsData(rawData, draw) {
         date = outputFormat(date);
         if (!data[date]) data[date] = {};
         let author = d.by;
+        //Reserve the phrase
+        phrases.forEach((p, i)=>{
+            let startIdx = d.title.toLowerCase().indexOf(p.toLowerCase())
+            if(startIdx>=0){
+                for (let j = 0; j < p.length; j++) {
+                    d.title[startIdx+j] = p[j];
+                }
+                d.title = d.title.replace(p.toLowerCase(), 'myphrase'+i);
+                debugger
+            }
+        });
+
         let words = d.title.split(' ');
         words = words.map(d => sanitizeWord((d)));
+        //Put the phrase back
+        phrases.forEach((p, i)=>{
+           words.forEach((w, j)=>{
+               if(w==="myphrase"+i) words[j] = p;
+           });
+        });
+
         words = removeStopWords(words, stopWords);
         words = words.filter(t=>t!='');//remove empty words
         words.forEach(word => {
